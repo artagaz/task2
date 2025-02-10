16a1ad173bb0fc61c62f85d860cd766abaa87e2f
artagaz <and.vaylin@gmail.com>
2025-02-10 08:43:48 +0700

    3

---
+++ main.py
@@ -0,0 +1,32 @@
+#1
+# inStr = input()
+#
+# print("1:", inStr[2])
+#
+# print("2:", inStr[-2])
+#
+# print("3:", inStr[0:5])
+#
+# print("4:", inStr[0:len(inStr)-2])
+#
+# print("5:", inStr[::2])
+# print("6:", inStr[1::2])
+#
+# print("7:", inStr[::-1])
+#
+# print("8:", inStr[::-2])
+#
+# print("9:", len(inStr))
+from operator import indexOf
+
+#2
+# inStr = input()
+# l = len(inStr) + 1
+# print (inStr[0:l//2])
+# print (inStr[l//2:])
+
+#3
+# inStr = input()
+# print(inStr[inStr.find("h")+1:inStr.find("h", inStr.find("h")+1, )][::-1])
+
+#4
